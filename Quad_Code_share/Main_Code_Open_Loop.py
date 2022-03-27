47c05688c1dad8319d50e032376bccf64c517cb6
EvanHope <hopee1@tcnj.edu>
2022-03-27 12:40:10 -0400

    fixed print statement

--- Quad_Code_share/Main_Code_Open_Loop.py
+++ Quad_Code_share/Main_Code_Open_Loop.py
@@ -201,7 +201,7 @@ ground_alt = 0
 target_alt = 10 #target alt experimentation.
 if baro.PRES < 1013: # small check in case barometer pressure is invalid
 	ground_alt = 44330.77*(1-(baro.PRES*100/101326)**0.1902632)
-	print("altitude ground?:" + ground_alt)
+	print("altitude ground?:" , ground_alt)
 	target_alt = target_alt + ground_alt
 led.setColor('Red')
 time.sleep(1)
