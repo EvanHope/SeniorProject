84d25f3de9857607acda866b5de6b2f59dd39d54
EvanHope <hopee1@tcnj.edu>
2022-03-27 14:45:00 -0400

    removed more prints

--- Quad_Code_share/Main_Code_Open_Loop.py
+++ Quad_Code_share/Main_Code_Open_Loop.py
@@ -360,9 +360,9 @@ while True:
 		elif (baro_timer == 5):
 			baro.calculatePressureAndTemperature()
 			baro_timer = 0
-			print(baro.PRES)
+			#print(baro.PRES)
 			if baro.PRES < 1013 and baro.PRES > 0: # Only update if barometer is valid
-				print(baro.PRES*100/101326)
+				#print(baro.PRES*100/101326)
 				alts = 44330.77*(1-(baro.PRES*100/101326)**0.1902632)
 				#print ("altitude?:", alts)
 				#alts = 0
