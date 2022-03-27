e50fd8abe139fa0d5e15d0e5f01069efce973f0c
EvanHope <hopee1@tcnj.edu>
2022-03-27 14:43:56 -0400

    removed print statements

--- Quad_Code_share/Main_Code_Open_Loop.py
+++ Quad_Code_share/Main_Code_Open_Loop.py
@@ -364,11 +364,11 @@ while True:
 			if baro.PRES < 1013 and baro.PRES > 0: # Only update if barometer is valid
 				print(baro.PRES*100/101326)
 				alts = 44330.77*(1-(baro.PRES*100/101326)**0.1902632)
-				print ("altitude?:", alts)
+				#print ("altitude?:", alts)
 				#alts = 0
 				prev_alt = current_alt
 				current_alt = alts - ground_alt
-				print("current alt: ", current_alt)
+				#print("current alt: ", current_alt)
 				
 		#buffer = GPS.bus.xfer2([100])
 		## GPS is disabled ##
