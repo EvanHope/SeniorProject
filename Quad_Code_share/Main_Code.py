dca2e03599dab9456b6bec556081051aa77fdbf9
EvanHope <hopee1@tcnj.edu>
2022-04-25 10:47:05 -0400

    added print statement

--- Quad_Code_share/Main_Code.py
+++ Quad_Code_share/Main_Code.py
@@ -576,12 +576,15 @@ while True:
 			
 			
 			yawProportional = kpy * deg2Rad(yawRateError)	
+			print(yawProportional)
 			#derivative = kd * deg2Rad((rollError - rollErrorPrev)/timeStep)
 			yawDerivative = kdy * -deg2Rad(rates[2])
+			print(yawDerivative)
 			#print(deg2Rad((rollError - rollErrorPrev)/timeStep))
 			yawErrorSum = yawErrorSum + (yawRateError + yawRateErrorPrev)*(timeStep/2.0)
 			#print(rollErrorSum)
 			yawIntegral = ki * deg2Rad(yawErrorSum)
+			print(yawIntegral)
 			
 			
 			rollProportional = kp * deg2Rad(rollError)
