23f908dee5fb5f19fa07c7400ad839d70adab977
EvanHope <hopee1@tcnj.edu>
2022-05-01 15:52:42 -0400

    removed yaw controller from adding to final motor values

--- Quad_Code_share/Main_Code.py
+++ Quad_Code_share/Main_Code.py
@@ -628,15 +628,15 @@ while True:
 
 			counter = counter + 1
 
-			#motor_right = throttle - Proll
-			#motor_left = throttle + Proll
-			#motor_front = throttle + Ppitch
-			#motor_back = throttle - Ppitch
+			motor_right = throttle - Proll
+			motor_left = throttle + Proll
+			motor_front = throttle + Ppitch
+			motor_back = throttle - Ppitch
 
-			motor_right = throttle - Proll - Pyaw
-			motor_left = throttle + Proll - Pyaw
-			motor_front = throttle + Ppitch + Pyaw
-			motor_back = throttle - Ppitch + Pyaw
+			#motor_right = throttle - Proll - Pyaw
+			#motor_left = throttle + Proll - Pyaw
+			#motor_front = throttle + Ppitch + Pyaw
+			#motor_back = throttle - Ppitch + Pyaw
 
 			zeroed = True
 		else:
