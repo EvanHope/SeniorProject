0980f7187d49988d2d81e34506fa97e7c8cca3a4
EvanHope <hopee1@tcnj.edu>
2022-05-04 09:56:19 -0400

    added preview program

--- Quad_Code_share/Camera_Preview.py
+++ Quad_Code_share/Camera_Preview.py
@@ -5,6 +5,6 @@ camera = PiCamera()
 
 camera.rotation = 180
 camera.start_preview()
-sleep(200)
+sleep(600)
 #camera.capture('/home/pi/Navio2/SeniorProject/Quad_Code_share/Pictures/image.jpg')
 camera.stop_preview()
