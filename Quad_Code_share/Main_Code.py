1e3428cea63ef9e77df8c4790575861f1c3447d9
EvanHope <hopee1@tcnj.edu>
2022-05-03 21:57:58 -0400

    yaw drift correction

--- Quad_Code_share/Main_Code.py
+++ Quad_Code_share/Main_Code.py
@@ -421,7 +421,8 @@ while True:
 		rates2[2] = -rates[2]
 		AHRS_data.update_imu(rates2, accels2)
 		yaw,roll,pitch = quat2euler(AHRS_data.quaternion,axes='rzxy')
-		yaw = yaw + 1.1
+		yaw = rad2Deg(yaw + .011)
+		yaw = deg2Rad(yaw)
 		baro_timer = baro_timer + 1
 		if (baro_timer == 1): baro.refreshPressure()
 		elif (baro_timer == 2): baro.readPressure()
